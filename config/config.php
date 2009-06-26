b08d5f7d8cde3cd811ec4dba061a37174b46f08f
Marek Dajnowski <marek@marek-laptop.(none)>
2009-06-26 18:55:57 +0100

    refactoring config

--- config/config.php
+++ config/config.php
@@ -1,16 +1,15 @@
 <?php
 
 error_reporting( E_ALL ^E_NOTICE );
-
-define( 'PROJECT_NAME', 'skeleton' ); // change me
 define( 'TIMER', microtime( true ) );
 define( 'PROJECT_PATH', substr( __file__, 0, strlen( __file__ ) - 18 ) );
+
+/* configuration begins */
+define( 'PROJECT_NAME', 'skeleton' ); // change me
 define( 'SMARTY_COMPILE_DIR', '/tmp/'. PROJECT_NAME );
 
-if( !file_exists( SMARTY_COMPILE_DIR ) )
-	mkdir( SMARTY_COMPILE_DIR );
-
-define( 'SMARTY_DIR', '/var/www/include/smarty/' );
+define( 'INCLUDE_PATH', '/var/www/include/' )
+define( 'SMARTY_DIR', INCLUDE_PATH .'smarty/' );
 define( 'SMARTY_TEMPLATES_DIR', PROJECT_PATH ."/templates/" );
 define( 'PRODUCTION', false );
 
@@ -20,5 +19,33 @@ define( 'DB_NAME', '' );
 define( 'DB_USERNAME', '' );
 define( 'DB_PASSWORD', '' );
 
-require_once( '/var/www/include/smarty/Smarty.class.php' );
-require_once( '/var/www/include/class/Entity.class.php' );
+/* end of configuration */
+
+if( !file_exists( INCLUDE_PATH .'/class/Entity.class.php' ) )
+{
+	die('LiteEntityLib not found, please follow <a href="http://www.sum-e.com/Page/Installation/#LiteEntityLib">instructions</a> to install it.');
+}
+elseif( !file_exists( INCLUDE_PATH .'/smarty/Smarty.class.php' ) )
+{
+	die('Smarty not found, please follow <a href="http://www.sum-e.com/Page/Installation/#Smarty">instructions</a> to install it.');
+}
+
+require_once( INCLUDE_PATH .'smarty/Smarty.class.php' );
+
+if( !file_exists( SMARTY_COMPILE_DIR ) )
+	mkdir( SMARTY_COMPILE_DIR );
+
+function __autoload( $name )
+{
+	$path_array = array( 'classes/', 'entities/', 'controllers/', INCLUDE_PATH .'class/' );
+
+	foreach( $path_array as $path )
+	{
+		if( file_exists( $path . $name .'.class.php' ) )
+		{
+			include_once( $path . $name .'.class.php' );
+			return true;
+		}
+	}
+}
+
